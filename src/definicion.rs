e453a80c2c905c7aa8d322b7ebbd4261760e2ca5
Jonathan Muñoz <cm23059@ues.edu.sv>
2026-06-02 23:26:54 -0600

    Definicion del grafo actualizado

--- src/definicion.rs
+++ src/definicion.rs
@@ -1,16 +1,33 @@
+use petgraph::graph::{DiGraph, NodeIndex};
 use petgraph::dot::{Dot, Config};
-use petgraph::graph::Graph;
-pub fn definicion_grafo() -> Graph<&'static str, i32> {
-    let mut grafo = Graph::<&str, i32>::new();   
-    let a = grafo.add_node("A");
-    let b = grafo.add_node("B");
-    let c = grafo.add_node("C");
-    let d = grafo.add_node("D");  
-    grafo.add_edge(a, b, 5);
-    grafo.add_edge(a, c, 2);
-    grafo.add_edge(b, d, 1);
-    grafo.add_edge(c, d, 7);
-    println!("{:?}", Dot::with_config(&grafo, &[Config::EdgeNoLabel]));
-    
-    grafo
+//definicion de la estrutura, str para string(el nodo), y i32 para int(numero que guarda cara arista del grafo)
+pub struct RedDeTransporte{
+    pub grafo: DiGraph<&'static str, i32>,
+}
+
+//para inicializar un grafo vacio
+impl RedDeTransporte{
+    pub fn new() -> Self{
+        RedDeTransporte { 
+            grafo: DiGraph::<&'static str, i32>::new()
+         }
+    }
+}
+
+impl RedDeTransporte{
+    //para agregar un nuevo nodo al grafo
+    pub fn agregar_nodo(&mut self, nombre_del_nodo: &'static str) -> NodeIndex {
+        self.grafo.add_node(nombre_del_nodo)
+    }
+
+    //para conectar los nodos
+    pub fn conectar(&mut self, nodo_origen: NodeIndex, nodo_destino: NodeIndex, peso: i32) {
+        self.grafo.add_edge(nodo_origen, nodo_destino, peso);
+    }
+
+    //para ver el grafo
+    pub fn mostrar_grafo(&self){
+        println!("Grafo:");
+        println!("{:?}", Dot::with_config(&self.grafo, &[Config::EdgeNoLabel]));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 mod definicion;
 fn main() {
-    let grafo = definicion::definicion_grafo();
-    println!("{:?}", grafo);
+let mut red_de_transporte = definicion::RedDeTransporte::new();
+
+    // Recreando exactamente tu ejemplo:
+    let a = red_de_transporte.agregar_nodo("A");
+    let b = red_de_transporte.agregar_nodo("B");
+    let c = red_de_transporte.agregar_nodo("C");
+    let d = red_de_transporte.agregar_nodo("D");
+    
+    red_de_transporte.conectar(a, b, 1); // 0 -> 1
+    red_de_transporte.conectar(a, c, 1); // 0 -> 2
+    red_de_transporte.conectar(b, d, 1); // 1 -> 3
+    red_de_transporte.conectar(c, d, 1); // 2 -> 3
+
+    red_de_transporte.mostrar_grafo();
 }
